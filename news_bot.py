7192dfc2e75e88dab6356fb0d578a0db33f23b32
daruiaauster-rayyeh <rayyeh.2025@gmail.com>
2025-07-04 15:58:46 +0800

    Update news_bot.py

--- news_bot.py
+++ news_bot.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 
 # 🔑 從環境變數讀取金鑰
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-GAS_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxbF-tnMU2B7fr5V3PHMlW7LfM3lQal98a7ioDSgjc/dev"
+GAS_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzRhVzLo0MBu-yL9xKRnPsvwfqG8jujCp2edud_p96q5dWJ5CNDmzfcJDIhaZxpNGEu/exec"
 
 # 配置 Gemini API
 genai.configure(api_key=GOOGLE_API_KEY)
